fa8e422b6ccd741863ba2af4656b32e97aa0fe74
amartya <arvyagami@proton.me>
2024-02-28 19:02:56 +0530

    ui overhaul for ItemEntry, and app welcome screen with name

---
+++ Mandi Mitra/WelcomeAndName.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeAndName.swift
+//  Mandi Mitra
+//
+//  Created by Amartya Yadav on 28/02/24.
+//
+
+import SwiftUI
+
+struct WelcomeAndName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeAndName()
+}
