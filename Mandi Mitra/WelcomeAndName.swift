44ecca193e60331db4c3aab14800b21413208eb6
amartya <arvyagami@proton.me>
2024-02-29 15:29:25 +0530

    polished the WelcomeAndName UI

--- Mandi Mitra/WelcomeAndName.swift
+++ Mandi Mitra/WelcomeAndName.swift
@@ -14,8 +14,14 @@ struct WelcomeAndName: View {
     
     var body: some View {
         VStack {
+            Image("final_app_icon") // Replace "appIcon" with your actual icon name
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 80)
+            Text("Welcome to Mandi Mitra")
+                .font(.title)
                     TextField("Enter your name", text: $userName)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
 
                     Button("Continue") {
