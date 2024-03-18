c20a9de4fe51d0a8ff42d73b0031297f3b68cc2f
amartya <arvyagami@proton.me>
2024-03-18 23:47:51 +0530

    Update View Model to check for updates and  return if update is required

--- Mandi Mitra/ViewModels/UpdateCheckerViewModel.swift
+++ Mandi Mitra/ViewModels/UpdateCheckerViewModel.swift
@@ -10,7 +10,16 @@ import Foundation
 class UpdateCheckerViewModel: ObservableObject {
     private var updateChecker = UpdateChecker()
     
+    @Published var isUpdateAvailable = false
+    
     func checkForUpdates() {
-        updateChecker.getLatestVersionFromServer()
+        updateChecker.getLatestVersionFromServer { latestVersion in
+            DispatchQueue.main.async {
+                if let latestVersion = latestVersion {
+                    let currentVersion = self.updateChecker.getCurrentlyInstalledVersion()
+                    self.isUpdateAvailable = self.updateChecker.isUpdateRequired(currentlyInstalledVersion: currentVersion, latestavailableVersion: latestVersion)
+                }
+            }
+        }
     }
 }
