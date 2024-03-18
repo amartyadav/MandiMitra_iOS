366da16acbb7a1de5bd05dc027be1819a5da9f82
amartya <arvyagami@proton.me>
2024-03-18 23:47:14 +0530

    implement all functions of UpdateChecker

--- Mandi Mitra/Utilities/UpdateChecker.swift
+++ Mandi Mitra/Utilities/UpdateChecker.swift
@@ -8,25 +8,29 @@
 import Foundation
 
 struct UpdateChecker {
-    public func getLatestVersionFromServer() {
+    public func getLatestVersionFromServer(completion: @escaping (String?) -> Void) {
         guard let url = URL(string: "https://mandimitraversioninfoserver.azurewebsites.net/get_ios_version") else {
             print("Invalid URL")
+            completion(nil)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print("API Error: \(error.localizedDescription)")
+                completion(nil)
                 return
             }
             
             guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
                 print("Server responded with an error")
+                completion(nil)
                 return
             }
             
             guard let data = data else {
                 print("No data received from server")
+                completion(nil)
                 return
             }
             
@@ -34,28 +38,28 @@ struct UpdateChecker {
                 // decoding the json data from the api
                 let decoder = JSONDecoder()
                 let versionInfo = try decoder.decode(VersionInfo.self, from: data)
+                completion(versionInfo.version)
                 
                 print("Latest version on server: \(versionInfo.version)")
             }
             
             catch {
                 print("Failed to decode JSON from API: \(error)")
+                completion(nil)
             }
-            
-            
         }
         
         task.resume()
     }
     
     func getCurrentlyInstalledVersion() -> String {
-        let currentlyInstalledVersion = "1.0.2"
-        // implement this method to dynamically get the current version of the app installed on the device
+        guard let currentlyInstalledVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return "Unknown"
+        }
         return currentlyInstalledVersion
     }
     
     func isUpdateRequired(currentlyInstalledVersion: String, latestavailableVersion: String) -> Bool {
-        let isUpdateRequired = false
         if currentlyInstalledVersion != latestavailableVersion {
             return true
         }
