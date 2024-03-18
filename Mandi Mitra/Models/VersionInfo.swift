ed05ec882538ebf81e5fb963b1c635b31098690a
amartya <arvyagami@proton.me>
2024-03-18 22:32:54 +0530

    Add VersionInfo struct to decode the API repsonse

---
+++ Mandi Mitra/Models/VersionInfo.swift
@@ -0,0 +1,13 @@
+//
+//  VersionInfo.swift
+//  Mandi Mitra
+//
+//  Created by Amartya Yadav on 18/03/24.
+//
+
+import Foundation
+
+struct VersionInfo: Codable {
+    let version: String
+    let date: String
+}
